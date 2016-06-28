d7ba5ae367754b64a41a1a87ba0dbe3e977343cc
Ashley Maceli and Kyle Wolfson <ashley@>
2016-06-28 14:17:52 -0700

    add on click listener to alert user on invalid entry or enter valid word to array list

--- app/src/main/java/com/epicodus/honorboggle/MainActivity.java
+++ app/src/main/java/com/epicodus/honorboggle/MainActivity.java
@@ -3,14 +3,22 @@ package com.epicodus.honorboggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mTextview;
-
+    private Button mSubmitWord;
+    private EditText mUserAnswer;
+    private List<String> validWords = new ArrayList<>();
 
     private List generateRandomString() {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -34,17 +42,53 @@ public class MainActivity extends AppCompatActivity {
         return returnList;
     }
 
+    private boolean lengthCheck(String answer) {
+        if (answer.length() < 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean letterCheck(String answer, String randomCharacters) {
+        boolean letterInvalid = true;
+        for (int i = 0; i < answer.length(); i ++) {
+            Character letter = answer.charAt(i);
+            String stringLetter = letter.toString();
+            if (randomCharacters.contains(stringLetter)) {
+                letterInvalid = false;
+            }
+        }
+        return letterInvalid;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-
-
-        String randomCharacters = generateRandomString().toString();
+        final String randomCharacters = generateRandomString().toString();
+        mSubmitWord = (Button) findViewById(R.id.submitWord);
+        mUserAnswer = (EditText) findViewById(R.id.userAnswer);
         mTextview = (TextView) findViewById(R.id.textView);
         mTextview.setText(randomCharacters);
 
-
+        mSubmitWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String answer = mUserAnswer.getText().toString();
+                if ( (!(lengthCheck(answer))) && ((letterCheck(answer, randomCharacters)))) {
+                    Toast.makeText(MainActivity.this, "Word must be at least 3 letters and contain only listed letters.", Toast.LENGTH_LONG).show();
+                } else if (!(lengthCheck(answer))) {
+                    Toast.makeText(MainActivity.this, "Word must be at least 3 letters.", Toast.LENGTH_LONG).show();
+                } else if ((letterCheck(answer, randomCharacters))) {
+                    Toast.makeText(MainActivity.this, "Word must contain only listed letters.", Toast.LENGTH_LONG).show();
+                } else {
+                    validWords.add(answer);
+                    Log.d("validWords", "validWords" + validWords.toString());
+                }
+            }
+        });
     }
 }
